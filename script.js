e588046d3902505d14f18d93eee81045622412ea
SalladShooter <139503084+SalladShooter@users.noreply.github.com>
2023-08-13 18:38:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function swap() {
+  var title = document.getElementById("title");
+  if (title.innerHTML === "Login") {
+    title.innerHTML = "Signup";
+  } else {
+    title.innerHTML = "Login";
+  }
+  var swap = document.getElementById("swap");
+  if (swap.innerHTML === "Don't Have An Account?") {
+    swap.innerHTML = "Have An Account?";
+  } else {
+    swap.innerHTML = "Don't Have An Account?";
+  }
+  var submit = document.getElementById("submit");
+  if (submit.innerHTML === "Sign-in") {
+    submit.innerHTML = "Sign-up";
+  } else {
+    submit.innerHTML = "Sign-in";
+  }
+}
 
+document.getElementById("swap").addEventListener("click", swap);
